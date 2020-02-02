a1b98fbc99010358076ca4e11ba0cac04708d71f
LiamHz <liam.hinzman@gmail.com>
2020-02-02 13:17:29 -0500

    Implemented wireframe rendering

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
+#include <vector>
 #include <cmath>
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+Model *model = NULL;
+const int width  = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
@@ -31,8 +37,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
         if (steep) {
             // If transposed, de-transpose
             image.set(y, x, color);
-        }
-        else {
+        } else {
             image.set(x, y, color);
         }
 
@@ -46,9 +51,29 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 int main(int argc, char** argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
-    line(20, 13, 40, 80, image, red);
-    line(80, 40, 13, 20, image, red);
+    if (2 == argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
+
+    TGAImage image(width, height, TGAImage::RGB);
+
+    // Wireframe rendering
+    // Draw lines read from coords in wavefront obj
+    for (int i=0; i < model -> nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j=0; j < 3; j++) {
+            Vec3f v0 = model -> vert(face[j]);
+            Vec3f v1 = model -> vert(face[(j+1)%3]);
+            int x0 = (v0.x+1.0) * width / 2.0;
+            int y0 = (v0.y+1.0) * width / 2.0;
+            int x1 = (v1.x+1.0) * width / 2.0;
+            int y1 = (v1.y+1.0) * width / 2.0;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
+
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     return 0;
